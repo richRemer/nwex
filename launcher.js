af4b7414af7aef1f47216dcf77b3294678da4350
Richard Remer <rich@zingle.me>
2015-09-27 17:35:42 -0700

    redirect to server after server start

--- launcher.js
+++ launcher.js
@@ -2,6 +2,12 @@ var browser = require("./lib/nwex-browser-model"),
     NwexServerBase = require("./lib/nwex-server");
 
 document.addEventListener("DOMContentLoaded", function() {
-    var NwexServer = browser(NwexServerBase, ko);
-    ko.applyBindings(new NwexServer(), document.getElementById("server"));
+    var NwexServer = browser(NwexServerBase, ko),
+        server = new NwexServer();
+
+    server.on("ready", function() {
+        window.location = "http://" + server.host + ":" + server.port + "/";
+    });
+
+    ko.applyBindings(server, document.getElementById("server"));
 });
